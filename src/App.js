02dcb61a6fb423eedb16b69316448ad6fefa5fb2
Jorge Navarro <jorge.navarro@payscale.com>
2022-05-13 20:57:15 -0400

    weiner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import bitchTits1 from './choi.jpeg';
+import bitchTits2 from './choi2.jpeg';
+import bitchTits3 from './choi3.jpeg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={bitchTits1} className="App-logo" alt="logo" />
+        <img src={bitchTits2} className="App-logo" alt="logo" />
+        <img src={bitchTits3} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          weird motherfucker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
